39f0d8a93a9cea4d03357873a15a469a458609dd
Azhan Khan <1byteword@gmail.com>
2019-06-17 15:56:17 -0500

    Add files via upload

---
+++ azure-vm.py
@@ -0,0 +1,14 @@
+import json
+import os
+from collections import OrderedDict
+
+with open('test.json', "r+") as jsonFile:
+	data = json.load(jsonFile, object_pairs_hook=OrderedDict)
+
+#	tmp = data["location"]
+	data["parameters"]["adminUsername"]["value"] = os.environ["username"]
+	data["parameters"]["adminPassword"]["value"] = os.environ["password"]
+
+	jsonFile.seek(0)
+	json.dump(data, jsonFile, indent=4)
+	jsonFile.truncate()
